da35c1d4498a9517001b8554b28079ebd8d0c9ec
Nithi-2008 <av2905@srmist.edu.in>
2026-03-17 08:27:56 +0530

    UC3 only file

---
+++ HelloAppuc3.java
@@ -0,0 +1,13 @@
+public class hellouser3 {
+    public static void main(String[] args) {
+        String name;
+
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            name = "World";
+        }
+
+        System.out.println("Hello, " + name + "!");
+    }
+}
